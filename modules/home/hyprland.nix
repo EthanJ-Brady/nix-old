294bbe98a0ef30d7f26cb313eee90bf24d9c355e
Ethan Brady <git@ethanbrady.xyz>
2024-03-18 15:50:10 -0600

    Actually added rofi shortcut this time

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -5,7 +5,7 @@
     wayland.windowManager.hyprland.settings = {
         "$mod" = "SUPER";
         bind = [
-            "$mod, SPACE, exec, wofi --show drun"
+            "$mod, SPACE, exec, rofi -show drun"
             "$mod, Return, exec, kitty"
             "$mod, Q, killactive"
             "$mod, F, exec, firefox"
