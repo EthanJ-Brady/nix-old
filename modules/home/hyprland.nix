12d1562979db56a323722a35620e483c3ec5db66
Ethan Brady <git@ethanbrady.xyz>
2024-03-19 00:05:18 -0600

    added the ability to switch between the default browser

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,15 +1,28 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-    wayland.windowManager.hyprland.enable = true;
-    wayland.windowManager.hyprland.settings = {
-        "$mod" = "SUPER";
-        bind = [
-            "$mod, SPACE, exec, rofi -show drun"
-            "$mod, Return, exec, kitty"
-            "$mod, Q, killactive"
-            "$mod, B, exec, vivaldi"
-            "$mod, D, exec, discord"
-        ];
+    options = {
+        hyprland = {
+            browser = lib.mkOption {
+                default = "vivaldi";
+                type = lib.types.str;
+                description = "The defualt browser to use in hyprland";
+            };
+        };
+    };
+    
+    config = {
+        wayland.windowManager.hyprland.enable = true;
+        wayland.windowManager.hyprland.settings = {
+            "$mod" = "SUPER";
+            "$meh" = "SHIFT + ALT + CTRL";
+            bind = [
+                "$mod, SPACE, exec, rofi -show drun"
+                "$mod, Return, exec, kitty"
+                "$mod, Q, killactive"
+                "$meh, B, exec, ${config.hyprland.browser}"
+                "$meh, D, exec, discord"
+            ];
+        };
     };
 }
