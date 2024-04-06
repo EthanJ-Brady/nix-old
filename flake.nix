b077072ba27c41f04c3cdaf9a67dfeb74365012c
Ethan Brady <git@ethanbrady.xyz>
2024-04-05 18:39:21 -0600

    Added darwin support

--- flake.nix
+++ flake.nix
@@ -1,22 +1,35 @@
 {
-  description = "Nixos config flake";
+    description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+        darwin = {
+            url = "github:LnL7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    outputs = { self, nixpkgs, darwin, ... }@inputs: {
+        nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [
+                ./hosts/default/configuration.nix
+                    inputs.home-manager.nixosModules.default
+            ];
+        };
+        darwinConfigurations.newton = darwin.lib.darwinSystem {
+            system = "aarch64-darwin";
+            specialArgs = {inherit inputs;};
+            modules = [
+                ./hosts/newton/configuration.nix
+                    # inputs.home-manager.nixosModules.default
+            ];
+        };
     };
-  };
 }
