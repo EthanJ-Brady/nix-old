433851a75b52dafe1218c50681e71f16d9de0d59
Ethan Brady <git@ethanbrady.xyz>
2024-04-27 17:38:17 -0600

    Refactored out home manger to it's own module so it can be built independently of nixconfig

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
         catppuccin.url = "github:catppuccin/nix";
 
         darwin = {
@@ -16,30 +17,47 @@
         };
     };
 
-    outputs = { self, nixpkgs, catppuccin, darwin, home-manager, ... }@inputs: {
-        nixosConfigurations.bernoulli = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs;};
-            modules = [
-                ./hosts/bernoulli/configuration.nix
-                home-manager.nixosModules.default
-                catppuccin.nixosModules.catppuccin
-                {
-                    home-manager = {
-                        users."ethan".imports = [
-                            ./hosts/bernoulli/home.nix
-                        ];
-                        useGlobalPkgs = true;
-                    };
-                }
-            ];
+    outputs = { self, nixpkgs, catppuccin, darwin, home-manager, ... }@inputs: 
+    let
+        pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config = { allowUnfree = true; };
         };
-        darwinConfigurations.newton = darwin.lib.darwinSystem {
-            system = "aarch64-darwin";
-            specialArgs = {inherit inputs;};
-            modules = [
-                ./hosts/newton/configuration.nix
-                home-manager.darwinModules.default
-            ];
+    in {
+        nixosConfigurations = {
+            bernoulli = nixpkgs.lib.nixosSystem {
+                specialArgs = {inherit inputs;};
+                modules = [
+                    ./hosts/bernoulli/configuration.nix
+                    home-manager.nixosModules.default
+                    catppuccin.nixosModules.catppuccin
+                    {
+                        home-manager = {
+                            users."ethan".imports = [
+                                ./hosts/bernoulli/home.nix
+                            ];
+                            useGlobalPkgs = true;
+                        };
+                    }
+                ];
+            };
+            newton = darwin.lib.darwinSystem {
+                system = "aarch64-darwin";
+                specialArgs = {inherit inputs;};
+                modules = [
+                    ./hosts/newton/configuration.nix
+                    home-manager.darwinModules.default
+                ];
+            };
+        };
+
+        homeConfigurations = {
+            bernoulli = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [
+                    ./hosts/bernoulli/home.nix
+                ];
+            };
         };
     };
 }
