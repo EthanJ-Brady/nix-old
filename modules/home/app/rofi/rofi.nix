6c19d204c5b69b5aeba6ca68da2e2498541493d0
Ethan Brady <git@ethanbrady.xyz>
2024-04-30 15:47:31 -0600

    Removed unused modules

--- modules/home/app/rofi/rofi.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-    home.packages = with pkgs; [
-        rofi
-    ];
-    home.file.".config/rofi/config.rasi".source = ./catppuccin/basic/.config/rofi/config.rasi;
-    home.file.".local/share/rofi/themes/catppuccin-mocha.rasi".source = ./catppuccin/basic/.local/share/rofi/themes/catppuccin-mocha.rasi;
-}
