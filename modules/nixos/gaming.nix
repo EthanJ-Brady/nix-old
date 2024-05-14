9096c62fb8429e6de71f3eda4ce8cb1d77861386
Ethan Brady <git@ethanbrady.xyz>
2024-05-13 21:28:11 -0600

    Added a new udev rule that disables DS Edge touchpad

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -12,6 +12,7 @@
     # Disables touchpad on Playstation controllers
     services.udev.extraRules = ''
         ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+        ATTRS{name}=="Sony Interactive Entertainment DualSense Edge Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
         ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
     '';
 }
