c5da431c18f90b7a54b0b20b645a98539a4ff149
Ethan Brady <git@ethanbrady.xyz>
2024-05-16 09:18:50 -0600

    Added kitty config and setup newton to use it

--- hosts/newton/home.nix
+++ hosts/newton/home.nix
@@ -3,9 +3,11 @@
 {
     imports = [
         ../../modules/home/app/neovim.nix
-        ../../modules/home/terminal/starship.nix
+        ../../modules/home/tools/starship.nix
+        ../../modules/home/app/kitty.nix
         catppuccin.homeManagerModules.catppuccin
     ];
 
+    catppuccin.flavour = "frappe";
     home.stateVersion = "23.11";
 }
