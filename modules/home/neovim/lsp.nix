e7ace788a7ff1386e6e163b779e81e56398df7f7
Ethan Brady <git@ethanbrady.xyz>
2024-05-21 11:32:09 -0600

    Feature : Nixvim
    Setup a more robust lsp setup for nixvim

--- modules/home/neovim/lsp.nix
+++ modules/home/neovim/lsp.nix
@@ -6,6 +6,7 @@
             enable = true;
             servers = {
                 html.enable = true;
+                lua-ls.enable = true;
                 nil_ls.enable = true;
                 pyright.enable = true;
                 tsserver.enable = true;
@@ -15,11 +16,20 @@
         cmp = {
             enable = true;
             autoEnableSources = true;
-            # sources = [ 
-            #     { name = "nvim_lsp"; }
-            #     { name = "path"; }
-            #     { name = "buffer"; }
-            # ];
+            settings = {
+                sources = [
+                    {name = "buffer";}
+                    {name = "path";}
+                    {name = "nvim_lsp";}
+                    {name = "luasnip";}
+                ];
+                mapping = {
+                    "<C-e>" = "cmp.mapping.close()";
+                    "<C-y>" = "cmp.mapping.confirm({ select = true })";
+                    "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                    "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                };
+            };
         };
     };
 }
