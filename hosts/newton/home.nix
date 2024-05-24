e27d6085a0bb238d941a7a416e78fa3eb9d51dbe
Ethan Brady <git@ethanbrady.xyz>
2024-05-24 17:11:24 -0600

    Home : Newton
    Updated path to the kitty config

--- hosts/newton/home.nix
+++ hosts/newton/home.nix
@@ -4,7 +4,7 @@
     imports = [
         ../../modules/home/neovim/default.nix
         ../../modules/home/tools/starship.nix
-        ../../modules/home/app/kitty.nix
+        ../../modules/home/programs/kitty.nix
         inputs.catppuccin.homeManagerModules.catppuccin
     ];
 
